d17f9ca90a99e6e5604a7ef200ae83084734ab5a
MikaelGRA <mikaelgra@gmail.com>
2015-12-29 18:19:35 +0100

    downgraded to .NET 4.5 downgraded to JSON 6.0.4

--- src/Vibrant.InfluxDB.Client/Properties/AssemblyInfo.cs
+++ src/Vibrant.InfluxDB.Client/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vibrant.InfluxDB.Client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An easy-to-use client for InfluxDB.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Vibrant A/S")]
 [assembly: AssemblyProduct("Vibrant.InfluxDB.Client")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Vibrant A/S 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
