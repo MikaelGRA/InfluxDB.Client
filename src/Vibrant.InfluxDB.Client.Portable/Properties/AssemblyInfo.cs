19aeb313fc14ba1e655bcee74c835f55f0bee366
MikaelGRA <mikaelgra@gmail.com>
2016-01-01 01:23:04 +0100

    AssemblyInfo version bumped

--- src/Vibrant.InfluxDB.Client.Portable/Properties/AssemblyInfo.cs
+++ src/Vibrant.InfluxDB.Client.Portable/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vibrant.InfluxDB.Client.Portable")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Vibrant.InfluxDB.Client")]
+[assembly: AssemblyDescription( "An easy-to-use client for InfluxDB." )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vibrant.InfluxDB.Client.Portable")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Vibrant A/S")]
+[assembly: AssemblyProduct("Vibrant.InfluxDB.Client")]
+[assembly: AssemblyCopyright("Copyright © Vibrant A/S 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
