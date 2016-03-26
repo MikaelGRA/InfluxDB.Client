363ec3f1fa27553c3f89032886dfc56192800cc3
MikaelGRA <mikaelgra@gmail.com>
2016-03-26 21:03:50 +0100

    first linq implementation working

--- test/Vibrant.InfluxDB.Client.ConsoleApp/Program.cs
+++ test/Vibrant.InfluxDB.Client.ConsoleApp/Program.cs
@@ -11,7 +11,7 @@ namespace Vibrant.InfluxDB.Client.ConsoleApp
       {
          var client = new InfluxClient( new Uri( "http://13.95.159.186:8086" ), "root", "root" );
 
-         var query = client.Query<ComputerInfo>( "ssd", "myMeasurement" )
+         var query = client.Query<ComputerInfo>( "mydb", "myMeasurement" )
             .Where( x => x.CPU >= 40.0 )
             .Where( x => x.RAM == 5000 );
 
