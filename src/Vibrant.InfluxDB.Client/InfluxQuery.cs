b98cc8b149c7a854c34f29468018f05bbab83600
MikaelGRA <mikaelgra@gmail.com>
2016-10-09 15:12:05 +0200

    removed some unused code

--- src/Vibrant.InfluxDB.Client/InfluxQuery.cs
+++ src/Vibrant.InfluxDB.Client/InfluxQuery.cs
@@ -1,87 +1,87 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Reflection;
+//using System.Threading.Tasks;
 
-namespace Vibrant.InfluxDB.Client
-{
-   public class InfluxQuery<TElement> : IQueryable<TElement>, IOrderedQueryable<TElement>
-   {
-      private Expression _expression;
-      private InfluxQueryProvider _provider;
+//namespace Vibrant.InfluxDB.Client
+//{
+//   public class InfluxQuery<TElement> : IQueryable<TElement>, IOrderedQueryable<TElement>
+//   {
+//      private Expression _expression;
+//      private InfluxQueryProvider _provider;
 
-      public InfluxQuery( InfluxQueryProvider provider )
-      {
-         if( provider == null )
-            throw new ArgumentNullException( "provider" );
+//      public InfluxQuery( InfluxQueryProvider provider )
+//      {
+//         if( provider == null )
+//            throw new ArgumentNullException( "provider" );
 
-         _provider = provider;
-         _expression = Expression.Constant( this );
-      }
+//         _provider = provider;
+//         _expression = Expression.Constant( this );
+//      }
 
-      public InfluxQuery( InfluxQueryProvider provider, Expression expression )
-      {
-         if( provider == null )
-            throw new ArgumentNullException( "provider" );
+//      public InfluxQuery( InfluxQueryProvider provider, Expression expression )
+//      {
+//         if( provider == null )
+//            throw new ArgumentNullException( "provider" );
 
-         if( expression == null )
-            throw new ArgumentNullException( "expression" );
+//         if( expression == null )
+//            throw new ArgumentNullException( "expression" );
 
-         if( !typeof( IQueryable<TElement> ).GetTypeInfo().IsAssignableFrom( expression.Type.GetTypeInfo() ) )
-            throw new ArgumentOutOfRangeException( "expression" );
+//         if( !typeof( IQueryable<TElement> ).GetTypeInfo().IsAssignableFrom( expression.Type.GetTypeInfo() ) )
+//            throw new ArgumentOutOfRangeException( "expression" );
 
-         _provider = provider;
-         _expression = expression;
-      }
+//         _provider = provider;
+//         _expression = expression;
+//      }
 
-      public Type ElementType
-      {
-         get
-         {
-            return typeof( TElement );
-         }
-      }
+//      public Type ElementType
+//      {
+//         get
+//         {
+//            return typeof( TElement );
+//         }
+//      }
 
-      public Expression Expression
-      {
-         get
-         {
-            return _expression;
-         }
-      }
+//      public Expression Expression
+//      {
+//         get
+//         {
+//            return _expression;
+//         }
+//      }
 
-      public IQueryProvider Provider
-      {
-         get
-         {
-            return _provider;
-         }
-      }
+//      public IQueryProvider Provider
+//      {
+//         get
+//         {
+//            return _provider;
+//         }
+//      }
 
-      public IEnumerator<TElement> GetEnumerator()
-      {
-         return ( (IEnumerable<TElement>)_provider.Execute( _expression ) ).GetEnumerator();
-      }
+//      public IEnumerator<TElement> GetEnumerator()
+//      {
+//         return ( (IEnumerable<TElement>)_provider.Execute( _expression ) ).GetEnumerator();
+//      }
 
-      IEnumerator IEnumerable.GetEnumerator()
-      {
-         return ( (IEnumerable)_provider.Execute( _expression ) ).GetEnumerator();
-      }
+//      IEnumerator IEnumerable.GetEnumerator()
+//      {
+//         return ( (IEnumerable)_provider.Execute( _expression ) ).GetEnumerator();
+//      }
 
-      public override string ToString()
-      {
-         return QueryText;
-      }
+//      public override string ToString()
+//      {
+//         return QueryText;
+//      }
 
-      public string QueryText
-      {
-         get
-         {
-            return _provider.GetQueryText( _expression );
-         }
-      }
-   }
-}
+//      public string QueryText
+//      {
+//         get
+//         {
+//            return _provider.GetQueryText( _expression );
+//         }
+//      }
+//   }
+//}
--- src/Vibrant.InfluxDB.Client/InfluxQueryProvider.cs
+++ src/Vibrant.InfluxDB.Client/InfluxQueryProvider.cs
@@ -1,153 +1,153 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Threading.Tasks;
-using Vibrant.InfluxDB.Client.Dto;
-using Vibrant.InfluxDB.Client.Helpers;
-using Vibrant.InfluxDB.Client.Linq;
-using Vibrant.InfluxDB.Client.Visitors;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Reflection;
+//using System.Threading.Tasks;
+//using Vibrant.InfluxDB.Client.Dto;
+//using Vibrant.InfluxDB.Client.Helpers;
+//using Vibrant.InfluxDB.Client.Linq;
+//using Vibrant.InfluxDB.Client.Visitors;
 
-namespace Vibrant.InfluxDB.Client
-{
-   public class InfluxQueryProvider : IQueryProvider
-   {
-      private static readonly MethodInfo ExecuteByRowMethod = typeof( InfluxQueryProvider ).GetTypeInfo().DeclaredMethods.First( x => x.Name == "ExecuteByRowType" );
-      private static readonly MethodInfo GetQueryTextByRowTypeMethod = typeof( InfluxQueryProvider ).GetTypeInfo().DeclaredMethods.First( x => x.Name == "GetQueryTextByRowType" );
-      private static readonly MethodInfo PerformProjectionMethod = typeof( InfluxQueryProvider ).GetTypeInfo().DeclaredMethods.First( x => x.Name == "PerformProjection" );
+//namespace Vibrant.InfluxDB.Client
+//{
+//   public class InfluxQueryProvider : IQueryProvider
+//   {
+//      private static readonly MethodInfo ExecuteByRowMethod = typeof( InfluxQueryProvider ).GetTypeInfo().DeclaredMethods.First( x => x.Name == "ExecuteByRowType" );
+//      private static readonly MethodInfo GetQueryTextByRowTypeMethod = typeof( InfluxQueryProvider ).GetTypeInfo().DeclaredMethods.First( x => x.Name == "GetQueryTextByRowType" );
+//      private static readonly MethodInfo PerformProjectionMethod = typeof( InfluxQueryProvider ).GetTypeInfo().DeclaredMethods.First( x => x.Name == "PerformProjection" );
 
-      private readonly InfluxClient _client;
-      private readonly Type _originalType;
-      private readonly string _measurementName;
-      private readonly string _db;
+//      private readonly InfluxClient _client;
+//      private readonly Type _originalType;
+//      private readonly string _measurementName;
+//      private readonly string _db;
 
-      public InfluxQueryProvider( Type originalType, InfluxClient client, string db, string measurementName )
-      {
-         _originalType = originalType;
-         _client = client;
-         _measurementName = measurementName;
-         _db = db;
-      }
+//      public InfluxQueryProvider( Type originalType, InfluxClient client, string db, string measurementName )
+//      {
+//         _originalType = originalType;
+//         _client = client;
+//         _measurementName = measurementName;
+//         _db = db;
+//      }
 
-      public IQueryable CreateQuery( Expression expression )
-      {
-         Type elementType = TypeHelper.GetElementType( expression.Type );
-         try
-         {
-            return (IQueryable)Activator.CreateInstance( typeof( InfluxQuery<> ).MakeGenericType( elementType ), new object[] { this, expression, _db, _measurementName } );
-         }
-         catch( TargetInvocationException tie )
-         {
-            throw tie.InnerException;
-         }
-      }
+//      public IQueryable CreateQuery( Expression expression )
+//      {
+//         Type elementType = TypeHelper.GetElementType( expression.Type );
+//         try
+//         {
+//            return (IQueryable)Activator.CreateInstance( typeof( InfluxQuery<> ).MakeGenericType( elementType ), new object[] { this, expression, _db, _measurementName } );
+//         }
+//         catch( TargetInvocationException tie )
+//         {
+//            throw tie.InnerException;
+//         }
+//      }
 
-      public IQueryable<TElement> CreateQuery<TElement>( Expression expression )
-      {
-         return new InfluxQuery<TElement>( this, expression );
-      }
+//      public IQueryable<TElement> CreateQuery<TElement>( Expression expression )
+//      {
+//         return new InfluxQuery<TElement>( this, expression );
+//      }
 
-      public object Execute( Expression expression )
-      {
-         try
-         {
-            return ExecuteByRowMethod.MakeGenericMethod( new[] { _originalType } ).Invoke( this, new[] { expression } );
-         }
-         catch( TargetInvocationException tie )
-         {
-            throw tie.InnerException;
-         }
-      }
+//      public object Execute( Expression expression )
+//      {
+//         try
+//         {
+//            return ExecuteByRowMethod.MakeGenericMethod( new[] { _originalType } ).Invoke( this, new[] { expression } );
+//         }
+//         catch( TargetInvocationException tie )
+//         {
+//            throw tie.InnerException;
+//         }
+//      }
 
-      public TResult Execute<TResult>( Expression expression )
-      {
-         return (TResult)Execute( expression );
-      }
+//      public TResult Execute<TResult>( Expression expression )
+//      {
+//         return (TResult)Execute( expression );
+//      }
 
-      internal string GetQueryText( Expression expression )
-      {
-         try
-         {
-            return (string)GetQueryTextByRowTypeMethod.MakeGenericMethod( new[] { _originalType } ).Invoke( this, new[] { expression } );
-         }
-         catch( TargetInvocationException tie )
-         {
-            throw tie.InnerException;
-         }
-      }
+//      internal string GetQueryText( Expression expression )
+//      {
+//         try
+//         {
+//            return (string)GetQueryTextByRowTypeMethod.MakeGenericMethod( new[] { _originalType } ).Invoke( this, new[] { expression } );
+//         }
+//         catch( TargetInvocationException tie )
+//         {
+//            throw tie.InnerException;
+//         }
+//      }
 
-      private InfluxQueryInfo<TInfluxRow> GetQueryInfo<TInfluxRow>( Expression expression )
-         where TInfluxRow : new()
-      {
-         expression = PartialEvaluator.Eval( expression, CanBeEvaluatedLocally );
-         return new InfluxQueryInfoGenerator<TInfluxRow>().GetInfo( expression, _db, _measurementName );
-      }
+//      private InfluxQueryInfo<TInfluxRow> GetQueryInfo<TInfluxRow>( Expression expression )
+//         where TInfluxRow : new()
+//      {
+//         expression = PartialEvaluator.Eval( expression, CanBeEvaluatedLocally );
+//         return new InfluxQueryInfoGenerator<TInfluxRow>().GetInfo( expression, _db, _measurementName );
+//      }
 
-      private string GetQueryTextByRowType<TInfluxRow>( Expression expression )
-         where TInfluxRow : new()
-      {
-         return GetQueryInfo<TInfluxRow>( expression ).GenerateInfluxQL();
-      }
+//      private string GetQueryTextByRowType<TInfluxRow>( Expression expression )
+//         where TInfluxRow : new()
+//      {
+//         return GetQueryInfo<TInfluxRow>( expression ).GenerateInfluxQL();
+//      }
 
-      private object ExecuteByRowType<TInfluxRow>( Expression expression )
-         where TInfluxRow : new()
-      {
-         var queryInfo = GetQueryInfo<TInfluxRow>( expression );
+//      private object ExecuteByRowType<TInfluxRow>( Expression expression )
+//         where TInfluxRow : new()
+//      {
+//         var queryInfo = GetQueryInfo<TInfluxRow>( expression );
 
-         var iql = queryInfo.GenerateInfluxQL();
+//         var iql = queryInfo.GenerateInfluxQL();
 
-         // Also need async version of this
-         var result = _client.ReadAsync<TInfluxRow>( _db, iql ).GetAwaiter().GetResult();
-         var enumerable = result.Results.FirstOrDefault()?.Series.FirstOrDefault()?.Rows;
+//         // Also need async version of this
+//         var result = _client.ReadAsync<TInfluxRow>( _db, iql ).GetAwaiter().GetResult();
+//         var enumerable = result.Results.FirstOrDefault()?.Series.FirstOrDefault()?.Rows;
 
-         var projection = queryInfo.SelectClause?.Projection;
-         if( projection == null )
-         {
-            if( enumerable != null )
-            {
-               return enumerable;
-            }
-            else
-            {
-               return new List<TInfluxRow>();
-            }
-         }
-         else
-         {
-            // Use alternative Read method using the provided projection
+//         var projection = queryInfo.SelectClause?.Projection;
+//         if( projection == null )
+//         {
+//            if( enumerable != null )
+//            {
+//               return enumerable;
+//            }
+//            else
+//            {
+//               return new List<TInfluxRow>();
+//            }
+//         }
+//         else
+//         {
+//            // Use alternative Read method using the provided projection
 
-            var returnElementType = TypeHelper.GetElementType( expression.Type );
-            return PerformProjectionMethod.MakeGenericMethod( new[] { returnElementType } ).Invoke( this, new object[] { projection, enumerable } );
-         }
-      }
+//            var returnElementType = TypeHelper.GetElementType( expression.Type );
+//            return PerformProjectionMethod.MakeGenericMethod( new[] { returnElementType } ).Invoke( this, new object[] { projection, enumerable } );
+//         }
+//      }
 
-      private List<TProjectedInfluxRow> PerformProjection<TProjectedInfluxRow>( RowProjection projection, IEnumerable<object> rows )
-      {
-         List<TProjectedInfluxRow> projectedRows = null;
-         if( rows != null )
-         {
-            projectedRows = projection.PerformProjections<TProjectedInfluxRow>( rows ).ToList();
-         }
-         else
-         {
-            projectedRows = new List<TProjectedInfluxRow>();
-         }
-         return projectedRows;
-      }
+//      private List<TProjectedInfluxRow> PerformProjection<TProjectedInfluxRow>( RowProjection projection, IEnumerable<object> rows )
+//      {
+//         List<TProjectedInfluxRow> projectedRows = null;
+//         if( rows != null )
+//         {
+//            projectedRows = projection.PerformProjections<TProjectedInfluxRow>( rows ).ToList();
+//         }
+//         else
+//         {
+//            projectedRows = new List<TProjectedInfluxRow>();
+//         }
+//         return projectedRows;
+//      }
 
-      private bool CanBeEvaluatedLocally( Expression expression )
-      {
-         if( expression is MethodCallExpression )
-         {
-            var mce = (MethodCallExpression)expression;
-            if( mce.Method.DeclaringType == typeof( InfluxFunctions ) )
-            {
-               return false;
-            }
-         }
+//      private bool CanBeEvaluatedLocally( Expression expression )
+//      {
+//         if( expression is MethodCallExpression )
+//         {
+//            var mce = (MethodCallExpression)expression;
+//            if( mce.Method.DeclaringType == typeof( InfluxFunctions ) )
+//            {
+//               return false;
+//            }
+//         }
 
-         return expression.NodeType != ExpressionType.Parameter;
-      }
-   }
-}
+//         return expression.NodeType != ExpressionType.Parameter;
+//      }
+//   }
+//}
