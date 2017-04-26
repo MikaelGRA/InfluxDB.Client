22f653facbf2b536f358d016e35bd39918db9acd
MikaelGRA <mikaelgra@gmail.com>
2017-04-26 13:59:38 +0200

    Fixed all tests so they never collide

--- src/Vibrant.InfluxDB.Client/InfluxResult.cs
+++ src/Vibrant.InfluxDB.Client/InfluxResult.cs
@@ -5,150 +5,156 @@ using System.Threading.Tasks;
 
 namespace Vibrant.InfluxDB.Client
 {
-   /// <summary>
-   /// Result of a query to influxdb that does not return a table.
-   /// </summary>
-   public class InfluxResult
-   {
-      public InfluxResult( int statementId, string error )
-      {
-         StatementId = statementId;
-         ErrorMessage = error;
-         Succeeded = error == null;
-      }
+    /// <summary>
+    /// Result of a query to influxdb that does not return a table.
+    /// </summary>
+    public class InfluxResult
+    {
+        public InfluxResult(int statementId, string error)
+        {
+            StatementId = statementId;
+            ErrorMessage = error;
+            Succeeded = error == null;
+        }
 
-      /// <summary>
-      /// Gets the error message, if the operation did not succeed.
-      /// </summary>
-      public string ErrorMessage { get; private set; }
+        /// <summary>
+        /// Gets the error message, if the operation did not succeed.
+        /// </summary>
+        public string ErrorMessage { get; private set; }
 
-      /// <summary>
-      /// Gets an indication of whether the operation succeeded.
-      /// </summary>
-      public bool Succeeded { get; private set; }
+        /// <summary>
+        /// Gets an indication of whether the operation succeeded.
+        /// </summary>
+        public bool Succeeded { get; private set; }
 
-      /// <summary>
-      /// Gets or sets the statement id.
-      /// </summary>
-      public int StatementId { get; private set; }
+        /// <summary>
+        /// Gets or sets the statement id.
+        /// </summary>
+        public int StatementId { get; private set; }
 
-      public void AppendErrorMessage( string message )
-      {
-         if( message != null )
-         {
-            if( ErrorMessage != null )
+        public void AppendErrorMessage(string message)
+        {
+            if (message != null)
             {
-               ErrorMessage += Environment.NewLine;
+                if (ErrorMessage != null)
+                {
+                    ErrorMessage += Environment.NewLine;
+                }
+                ErrorMessage += message;
+
+                Succeeded = false;
             }
-            ErrorMessage += message;
+        }
+    }
 
-            Succeeded = false;
-         }
-      }
-   }
+    /// <summary>
+    /// Result of a query to influxdb that returns one or more tables.
+    /// </summary>
+    /// <typeparam name="TInfluxRow"></typeparam>
+    public class InfluxResult<TInfluxRow> : InfluxResult
+    {
+        public InfluxResult(int statementId, string error)
+           : this(statementId, error, new List<InfluxSeries<TInfluxRow>>())
+        {
+            Series = new List<InfluxSeries<TInfluxRow>>();
+        }
 
-   /// <summary>
-   /// Result of a query to influxdb that returns one or more tables.
-   /// </summary>
-   /// <typeparam name="TInfluxRow"></typeparam>
-   public class InfluxResult<TInfluxRow> : InfluxResult
-   {
-      public InfluxResult( int statementId, string error )
-         : base( statementId, error )
-      {
-         Series = new List<InfluxSeries<TInfluxRow>>();
-      }
+        public InfluxResult(int statementId, string error, List<InfluxSeries<TInfluxRow>> rows)
+           : base(statementId, error)
+        {
+            Series = rows;
+        }
 
-      /// <summary>
-      /// Gets the series.
-      /// </summary>
-      public List<InfluxSeries<TInfluxRow>> Series { get; set; }
+        /// <summary>
+        /// Gets the series.
+        /// </summary>
+        public List<InfluxSeries<TInfluxRow>> Series { get; private set; }
 
-      /// <summary>
-      /// Finds the serie that can be identified by the specified tags. 
-      /// This method is obsolete and will be removed in the next major upcoming release. 
-      /// Use the overload that requires a name instead.
-      /// </summary>
-      /// <param name="tags"></param>
-      /// <returns></returns>
-      [Obsolete("Method will be removed in next major release. Use overload that requires a name.")]
-      public InfluxSeries<TInfluxRow> FindGroup( IEnumerable<KeyValuePair<string, object>> tags )
-      {
-         if( tags == null )
-            throw new ArgumentNullException( nameof( tags ) );
+        /// <summary>
+        /// Finds the serie that can be identified by the specified tags. 
+        /// This method is obsolete and will be removed in the next major upcoming release. 
+        /// Use the overload that requires a name instead.
+        /// </summary>
+        /// <param name="tags"></param>
+        /// <returns></returns>
+        [Obsolete("Method will be removed in next major release. Use overload that requires a name.")]
+        public InfluxSeries<TInfluxRow> FindGroup(IEnumerable<KeyValuePair<string, object>> tags)
+        {
+            if (tags == null)
+                throw new ArgumentNullException(nameof(tags));
 
-         if( Series.Any( x => x.GroupedTags == null ) )
-            throw new InvalidOperationException( "This query result set is not grouped." );
+            if (Series.Any(x => x.GroupedTags == null))
+                throw new InvalidOperationException("This query result set is not grouped.");
 
-         return FindGroupInternal( null, tags, false );
-      }
+            return FindGroupInternal(null, tags, false);
+        }
 
-      /// <summary>
-      /// Finds the serie that can be identified by the specified tags and name.
-      /// </summary>
-      /// <param name="seriesName"></param>
-      /// <param name="tags"></param>
-      /// <returns></returns>
-      public InfluxSeries<TInfluxRow> FindGroup( string seriesName, IEnumerable<KeyValuePair<string, object>> tags )
-      {
-         if( tags == null )
-            throw new ArgumentNullException( nameof( tags ) );
+        /// <summary>
+        /// Finds the serie that can be identified by the specified tags and name.
+        /// </summary>
+        /// <param name="seriesName"></param>
+        /// <param name="tags"></param>
+        /// <returns></returns>
+        public InfluxSeries<TInfluxRow> FindGroup(string seriesName, IEnumerable<KeyValuePair<string, object>> tags)
+        {
+            if (tags == null)
+                throw new ArgumentNullException(nameof(tags));
 
-         if( Series.Any( x => x.GroupedTags == null ) )
-            throw new InvalidOperationException( "This query result set is not grouped." );
+            if (Series.Any(x => x.GroupedTags == null))
+                throw new InvalidOperationException("This query result set is not grouped.");
 
-         return FindGroupInternal( seriesName, tags, true );
-      }
+            return FindGroupInternal(seriesName, tags, true);
+        }
 
-      public void AddInfluxSeries( InfluxSeries<TInfluxRow> series )
-      {
-         Series.Add( series );
-      }
+        public void AddInfluxSeries(InfluxSeries<TInfluxRow> series)
+        {
+            Series.Add(series);
+        }
 
-      internal InfluxSeries<TInfluxRow> FindGroupInternal( string seriesName, IEnumerable<KeyValuePair<string, object>> tags, bool requireNameComparison )
-      {
-         foreach( var result in Series )
-         {
-            if( Matches( result, seriesName, tags, requireNameComparison ) )
+        internal InfluxSeries<TInfluxRow> FindGroupInternal(string seriesName, IEnumerable<KeyValuePair<string, object>> tags, bool requireNameComparison)
+        {
+            foreach (var result in Series)
             {
-               return result;
+                if (Matches(result, seriesName, tags, requireNameComparison))
+                {
+                    return result;
+                }
             }
-         }
-         return null;
-      }
+            return null;
+        }
 
 
-      private bool Matches( InfluxSeries<TInfluxRow> result, string seriesName, IEnumerable<KeyValuePair<string, object>> tags, bool requireNameComparison )
-      {
-         if( requireNameComparison && result.Name != seriesName )
-         {
-            return false;
-         }
+        private bool Matches(InfluxSeries<TInfluxRow> result, string seriesName, IEnumerable<KeyValuePair<string, object>> tags, bool requireNameComparison)
+        {
+            if (requireNameComparison && result.Name != seriesName)
+            {
+                return false;
+            }
 
-         if( result.GroupedTags == null && ( tags == null || tags.Count() == 0 ) )
-         {
+            if (result.GroupedTags == null && (tags == null || tags.Count() == 0))
+            {
+                return true;
+            }
+
+            foreach (var tag in tags)
+            {
+                object tagValue;
+                if (result.GroupedTags.TryGetValue(tag.Key, out tagValue))
+                {
+                    if (tagValue != null)
+                    {
+                        if (!tagValue.Equals(tag.Value))
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return tag.Value == null;
+                    }
+                }
+            }
             return true;
-         }
-
-         foreach( var tag in tags )
-         {
-            object tagValue;
-            if( result.GroupedTags.TryGetValue( tag.Key, out tagValue ) )
-            {
-               if( tagValue != null )
-               {
-                  if( !tagValue.Equals( tag.Value ) )
-                  {
-                     return false;
-                  }
-               }
-               else
-               {
-                  return tag.Value == null;
-               }
-            }
-         }
-         return true;
-      }
-   }
+        }
+    }
 }
--- test/Vibrant.InfluxDB.Client.Tests/InfluxClientFixture.cs
+++ test/Vibrant.InfluxDB.Client.Tests/InfluxClientFixture.cs
@@ -7,136 +7,136 @@ using Vibrant.InfluxDB.Client.Rows;
 
 namespace Vibrant.InfluxDB.Client.Tests
 {
-   public sealed class InfluxClientFixture : IDisposable
-   {
-      private bool _disposed;
+    public sealed class InfluxClientFixture : IDisposable
+    {
+        private bool _disposed;
 
-      public const string DatabaseName = "unittestdb";
-      //public static readonly string InfluxHost = "http://winflux.westeurope.cloudapp.azure.com:8086";
-      //public static readonly string InfluxHost = "http://localhost:8086"; http://ipv4.fiddler
-      public static readonly string InfluxHost = "http://ipv4.fiddler:8086";
-      //public static readonly string InfluxHost = "http://52.174.58.40:8086";
+        public const string DatabaseName = "unittestdb";
+        //public static readonly string InfluxHost = "http://winflux.westeurope.cloudapp.azure.com:8086";
+        //public static readonly string InfluxHost = "http://localhost:8086"; http://ipv4.fiddler
+        public static readonly string InfluxHost = "http://ipv4.fiddler:8086";
+        //public static readonly string InfluxHost = "http://52.174.58.40:8086";
 
-      public InfluxClient Client { get; set; }
+        public InfluxClient Client { get; set; }
 
-      public InfluxClientFixture()
-      {
-         Client = new InfluxClient( new Uri( InfluxHost ), "root", "root" );
-         Client.CreateDatabaseAsync( DatabaseName ).Wait();
-      }
+        public InfluxClientFixture()
+        {
+            Client = new InfluxClient(new Uri(InfluxHost), "root", "root");
+            Client.CreateDatabaseAsync(DatabaseName).Wait();
+        }
 
-      public static readonly string[] Regions = new[] { "west-eu", "north-eu", "west-us", "east-us", "asia" };
-      public static readonly string[] Hosts = new[] { "ma-lt", "surface-book" };
-      public static readonly TestEnum1?[] TestEnums = new TestEnum1?[] { null, TestEnum1.Value1, TestEnum1.Value2, TestEnum1.Value3 };
+        public static readonly string[] Regions = new[] { "west-eu", "north-eu", "west-us", "east-us", "asia" };
+        public static readonly string[] Hosts = new[] { "ma-lt", "surface-book" };
+        public static readonly TestEnum1?[] TestEnums = new TestEnum1?[] { null, TestEnum1.Value1, TestEnum1.Value2, TestEnum1.Value3 };
 
-      public static ComputerInfo[] CreateTypedRowsStartingAt( DateTime start, int rows, bool includeNulls )
-      {
-         var rng = new Random();
+        public static ComputerInfo[] CreateTypedRowsStartingAt(DateTime start, int rows, bool includeNulls)
+        {
+            var rng = new Random();
 
-         var timestamp = start;
-         var infos = new ComputerInfo[ rows ];
-         for( int i = 0 ; i < rows ; i++ )
-         {
-            long ram = rng.Next( int.MaxValue );
-            double cpu = rng.NextDouble();
-            string region = Regions[ rng.Next( Regions.Length ) ];
-            string host = Hosts[ rng.Next( Hosts.Length ) ];
-
-            if( includeNulls )
+            var timestamp = start;
+            var infos = new ComputerInfo[rows];
+            for (int i = 0; i < rows; i++)
             {
-               var info = new ComputerInfo { Timestamp = timestamp, RAM = ram, Region = region };
-               infos[ i ] = info;
+                long ram = rng.Next(int.MaxValue);
+                double cpu = rng.NextDouble();
+                string region = Regions[rng.Next(Regions.Length)];
+                string host = Hosts[rng.Next(Hosts.Length)];
+
+                if (includeNulls)
+                {
+                    var info = new ComputerInfo { Timestamp = timestamp, RAM = ram, Region = region };
+                    infos[i] = info;
+                }
+                else
+                {
+                    var info = new ComputerInfo { Timestamp = timestamp, CPU = cpu, RAM = ram, Host = host, Region = region };
+                    infos[i] = info;
+                }
+
+                timestamp = timestamp.AddSeconds(1);
             }
-            else
+
+            return infos;
+        }
+
+        public static EnumeratedRow[] CreateEnumeratedRowsStartingAt(DateTime start, int rows)
+        {
+            var rng = new Random();
+
+            var timestamp = start;
+            var infos = new EnumeratedRow[rows];
+            for (int i = 0; i < rows; i++)
             {
-               var info = new ComputerInfo { Timestamp = timestamp, CPU = cpu, RAM = ram, Host = host, Region = region };
-               infos[ i ] = info;
+                var value = rng.NextDouble();
+                var type = TestEnums[rng.Next(TestEnums.Length)];
+
+                var info = new EnumeratedRow { Timestamp = timestamp, Value = value, Type = type };
+                infos[i] = info;
+
+                timestamp = timestamp.AddSeconds(1);
             }
 
-            timestamp = timestamp.AddSeconds( 1 );
-         }
+            return infos;
+        }
 
-         return infos;
-      }
+        public static DynamicInfluxRow[] CreateDynamicRowsStartingAt(DateTime start, int rows)
+        {
+            var rng = new Random();
 
-      public static EnumeratedRow[] CreateEnumeratedRowsStartingAt( DateTime start, int rows )
-      {
-         var rng = new Random();
+            var timestamp = start;
+            var infos = new DynamicInfluxRow[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                long ram = rng.Next(int.MaxValue);
+                double cpu = rng.NextDouble();
+                string region = Regions[rng.Next(Regions.Length)];
+                string host = Hosts[rng.Next(Hosts.Length)];
 
-         var timestamp = start;
-         var infos = new EnumeratedRow[ rows ];
-         for( int i = 0 ; i < rows ; i++ )
-         {
-            var value = rng.NextDouble();
-            var type = TestEnums[ rng.Next( TestEnums.Length ) ];
+                var info = new DynamicInfluxRow();
+                info.Fields.Add("cpu", cpu);
+                info.Fields.Add("ram", ram);
+                info.Tags.Add("host", host);
+                info.Tags.Add("region", region);
+                info.Timestamp = timestamp;
 
-            var info = new EnumeratedRow { Timestamp = timestamp, Value = value, Type = type };
-            infos[ i ] = info;
+                infos[i] = info;
 
-            timestamp = timestamp.AddSeconds( 1 );
-         }
+                timestamp = timestamp.AddSeconds(1);
+            }
+            return infos;
+        }
 
-         return infos;
-      }
+        #region IDisposable
 
-      public static DynamicInfluxRow[] CreateDynamicRowsStartingAt( DateTime start, int rows )
-      {
-         var rng = new Random();
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~InfluxClientFixture()
+        {
+            Dispose(false);
+        }
 
-         var timestamp = start;
-         var infos = new DynamicInfluxRow[ rows ];
-         for( int i = 0 ; i < rows ; i++ )
-         {
-            long ram = rng.Next( int.MaxValue );
-            double cpu = rng.NextDouble();
-            string region = Regions[ rng.Next( Regions.Length ) ];
-            string host = Hosts[ rng.Next( Hosts.Length ) ];
+        /// <summary>
+        /// Disposes the InfluxClient and the internal HttpClient that it uses.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                Dispose(true);
+                _disposed = true;
+                GC.SuppressFinalize(this);
+            }
+        }
 
-            var info = new DynamicInfluxRow();
-            info.Fields.Add( "cpu", cpu );
-            info.Fields.Add( "ram", ram );
-            info.Tags.Add( "host", host );
-            info.Tags.Add( "region", region );
-            info.Timestamp = timestamp;
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Client.DropDatabaseAsync(DatabaseName).Wait();
+            }
+        }
 
-            infos[ i ] = info;
-
-            timestamp = timestamp.AddSeconds( 1 );
-         }
-         return infos;
-      }
-
-      #region IDisposable
-
-      /// <summary>
-      /// Destructor.
-      /// </summary>
-      ~InfluxClientFixture()
-      {
-         Dispose( false );
-      }
-
-      /// <summary>
-      /// Disposes the InfluxClient and the internal HttpClient that it uses.
-      /// </summary>
-      public void Dispose()
-      {
-         if( !_disposed )
-         {
-            Dispose( true );
-            _disposed = true;
-            GC.SuppressFinalize( this );
-         }
-      }
-
-      private void Dispose( bool disposing )
-      {
-         if( disposing )
-         {
-            Client.DropDatabaseAsync( DatabaseName ).Wait();
-         }
-      }
-
-      #endregion
-   }
+        #endregion
+    }
 }
